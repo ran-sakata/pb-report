2172889558e2088625b7627e3f56d1f83669a804
ran-sakata <ran.sakata@designist-lab.com>
2025-04-20 21:10:21 +0900

    ルート変更

--- routes/web.php
+++ routes/web.php
@@ -22,24 +22,24 @@ Route::controller(HistoryController::class)->group(function () {
 Route::controller(FirstController::class)->group(function () {
     Route::get('/new', 'create')->name('report.create');
     Route::post('/new', 'store')->name('report.store');
-    Route::get('/{report}', 'edit')->name('report.edit');
-    Route::post('/{report}', 'update')->name('report.update');
+    Route::get('/edit/{report}', 'edit')->name('report.edit');
+    Route::post('/edit/{report}', 'update')->name('report.update');
 });
 
 Route::controller(SecondController::class)->group(function () {
-    Route::get('/{report}/second-page', 'index')->name('second-page');
-    Route::post('/{report}/second-page', 'update')->name('second-page.update');
+    Route::get('/edit/{report}/second-page', 'index')->name('second-page');
+    Route::post('/edit/{report}/second-page', 'update')->name('second-page.update');
 });
 
 Route::controller(ThirdController::class)->group(function () {
-    Route::get('/{report}/third-page', 'index')->name('third-page');
-    Route::post('/{report}/third-page', 'update')->name('third-page.update');
+    Route::get('/edit/{report}/third-page', 'index')->name('third-page');
+    Route::post('/edit/{report}/third-page', 'update')->name('third-page.update');
 });
 
 Route::controller(ForthController::class)->group(function () {
-    Route::get('/{report}/forth-page', 'index')->name('forth-page');
-    Route::post('/{report}/forth-page', 'update')->name('forth-page.update');
+    Route::get('/edit/{report}/forth-page', 'index')->name('forth-page');
+    Route::post('/edit/{report}/forth-page', 'update')->name('forth-page.update');
 });
 
-Route::get('/{report}/confirmation', ConfirmationController::class)->name('confirmation');
-Route::get('/{report}/download', DownloadController::class)->name('export');
+Route::get('/edit/{report}/confirmation', ConfirmationController::class)->name('confirmation');
+Route::get('/edit/{report}/download', DownloadController::class)->name('export');
